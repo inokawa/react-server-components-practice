9b3ac5f24f0abdef6e3c94d3942fffce809e7a5e
inokawa <48897392+inokawa@users.noreply.github.com>
2023-06-07 23:54:07 +0900

    Part 1 Step 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import { IncomingMessage, ServerResponse, createServer } from "http";
 import { readFile } from "fs/promises";
 import escapeHtml from "escape-html";
+import { ReactNode } from "react";
 
 createServer(async (_req, res) => {
   const author = "Jae Doe";
   const postContent = await readFile("./posts/hello-world.txt", "utf8");
   sendHTML(
     res,
-    `<html>
+    <html>
       <head>
         <title>My blog</title>
       </head>
@@ -16,15 +17,17 @@ createServer(async (_req, res) => {
           <a href="/">Home</a>
           <hr />
         </nav>
-        <article>
-          ${escapeHtml(postContent)}
-        </article>
+        <article>{postContent}</article>
         <footer>
-          <hr>
-          <p><i>(c) ${escapeHtml(author)}, ${new Date().getFullYear()}</i></p>
+          <hr />
+          <p>
+            <i>
+              (c) {author}, {new Date().getFullYear()}
+            </i>
+          </p>
         </footer>
       </body>
-    </html>`
+    </html>
   );
 }).listen(8080);
 
@@ -32,8 +35,40 @@ function sendHTML(
   res: ServerResponse<IncomingMessage> & {
     req: IncomingMessage;
   },
-  html: string
+  jsx: ReactNode
 ) {
+  const html = renderJSXToHTML(jsx);
   res.setHeader("Content-Type", "text/html");
   res.end(html);
+}
+
+function renderJSXToHTML(jsx: ReactNode): string {
+  if (typeof jsx === "string" || typeof jsx === "number") {
+    // This is a string. Escape it and put it into HTML directly.
+    return escapeHtml(String(jsx));
+  } else if (jsx == null || typeof jsx === "boolean") {
+    // This is an empty node. Don't emit anything in HTML for it.
+    return "";
+  } else if (Array.isArray(jsx)) {
+    // This is an array of nodes. Render each into HTML and concatenate.
+    return jsx.map((child) => renderJSXToHTML(child)).join("");
+  } else if (typeof jsx === "object") {
+    // Check if this object is a React JSX element (e.g. <div />).
+    if (jsx.$$typeof === Symbol.for("react.element")) {
+      // Turn it into an an HTML tag.
+      let html = "<" + jsx.type;
+      for (const propName in jsx.props) {
+        if (jsx.props.hasOwnProperty(propName) && propName !== "children") {
+          html += " ";
+          html += propName;
+          html += "=";
+          html += escapeHtml(jsx.props[propName]);
+        }
+      }
+      html += ">";
+      html += renderJSXToHTML(jsx.props.children);
+      html += "</" + jsx.type + ">";
+      return html;
+    } else throw new Error("Cannot render an object.");
+  } else throw new Error("Not implemented.");
 }
